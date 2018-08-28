c7f7598ea1d1a6ec80b647e276674b10878cfed4
Damien Plumettaz <damien.plumettaz@plumettaz.com>
2018-08-28 11:33:34 +0200

    initial commit

---
+++ pages/_text.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <canvas ref="canvas"></canvas>
+</div>
+</template>
+
+<script>
+import QRCode from 'qrcode';
+
+export default {
+  mounted() {
+    QRCode.toCanvas(this.$refs.canvas, this.$route.params.text || "hello");
+  }
+}
+</script>
